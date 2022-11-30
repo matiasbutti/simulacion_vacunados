7294196d7acd3c21fea650599970a9fcdeb514b0
matiasbutti <matias.butti@gmail.com>
2022-11-30 19:05:33 -0300

    se agrega el partido

--- src/simular_vacunados.R
+++ src/simular_vacunados.R
@@ -1,11 +1,24 @@
 library(tidyverse)
 library(lubridate)
 
-cant = 10000000
+deptos <- read_csv("deptos.csv", locale = locale(encoding = "windows-1252"))
+glimpse(deptos)
+unique(deptos$Provincia)
 
-vacunas <- rep(c("anticovid_pfizer", "anticovid_moderna", "antigripal_sanofi", "hepatitisb_gsk" ),each=cant/4)
-lotes <- rep(c(111,222,333,444,555,666,777,888,999),cant, replace=TRUE)
-lugar <- sample(c("San Luis", "Catamarca", "La Rioja", "Buenos Aires", "CABA" ),cant, replace=TRUE)
+cant = 10000000
+memory.size(max=1200000)
+vacunas_vector <- c("anticovid_pfizer", "anticovid_moderna", "antigripal_sanofi", "hepatitisb_gsk" )
+vacunas <- rep(vacunas_vector,each=cant/length(vacunas_vector))
+lotes <- sample(c(111,222,333,444,555,666,777,888,999),cant, replace=TRUE)
+
+#lugar <- sample(c("San Luis", "Catamarca", "La Rioja", "Buenos Aires", "CABA" ),cant, replace=TRUE)
+
+lugares_pos <- round(runif(cant, 1, 527), digits=0)
+pcia_ciudad <- deptos[lugares_pos, c(1,3)]
+provincias<-pcia_ciudad[1]
+ciudades<-pcia_ciudad[2]
+
+
 fechas_rango <- (seq(ymd('2021-01-01'),ymd('2022-11-01'), by = '1 day'))
 fechas <- sample(fechas_rango, cant, replace=TRUE)
 
@@ -16,6 +29,6 @@ fechas_nacimiento <- sample(fechas_nacimiento_rango, cant, replace=TRUE)
 
 sexo_al_nacer <- sample(c("M", "F" ),cant, replace=TRUE)
 
-df <- data.frame(vacunas, lotes, lugar, fechas, dnis)
+df <- data.frame(vacunas, lotes, provincias, ciudades, fechas, dnis, fechas_nacimiento, sexo_al_nacer)
 glimpse(df)
 write_csv(x = df, file = paste0("./", cant/1000000, "m.csv"))
